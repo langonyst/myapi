be27c32880c91053e2befc4c09142182377256dd
Langony ST <langonyst@gmail.com>
2023-06-16 12:13:44 -0300

    feat: adicionado rotas para crud

--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import { MoviesController } from '../controllers/MoviesController.js'
+import { validId } from '../middlewares/global.js'
 
 const movieRouter = express.Router()
 const moviesController = new MoviesController()
 
 movieRouter.get('/', moviesController.getMovies)
+movieRouter.get('/:id', validId, moviesController.getMovieById)
+movieRouter.post('/', moviesController.createMovie)
+movieRouter.patch('/:id', validId, moviesController.updateMovie)
+movieRouter.delete('/:id', validId, moviesController.deleteMovie)
 
 export default movieRouter

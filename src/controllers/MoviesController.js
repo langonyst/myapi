c8ce584df67d2059b97ecc3c73c612af2d3ffe9e
Langony ST <langonyst@gmail.com>
2023-06-16 12:12:57 -0300

    feat: adicionado funções de crud para controllers

--- src/controllers/MoviesController.js
+++ src/controllers/MoviesController.js
@@ -12,4 +12,86 @@ export class MoviesController {
             throw error;
         }
     }
+
+    async createMovie(req, res){
+        try{
+            const { title, year, rating, type, gender} = req.body
+
+            if(!title || !year || !rating || !type || !gender){
+                return res.status(400).send({
+                    message: 'Submit all fields to create a Movie!'
+                })
+            }
+
+            await movieModel.createMovie({
+                title,
+                year,
+                rating,
+                type,
+                gender
+            })
+
+            res.status(201).send({
+                message: 'Movie created successfully'
+            })
+
+        }catch(error){
+            console.error('Error creating a movie: ', error)
+            throw error
+        }
+    }
+
+    async getMovieById(req, res){
+        try{
+            const { id } = req.params
+            const movie = await movieModel.findMovieById(id)
+            res.json(movie)
+        }catch(error){
+            console.error('Error getting a movie: ', error)
+            throw error;
+        }
+    }
+
+    async updateMovie(req, res){
+        try{
+            const { id } = req.params
+            const { title, year, rating, type, gender} = req.body
+
+            if(!title && !year && !rating && !type && !gender){
+                return res.status(400).send({
+                    message: 'Submit at least one field to update the Movie!'
+                })
+            }
+            const docMovie = {
+                title,
+                year,
+                rating,
+                type,
+                gender
+            }
+
+            const movie = await movieModel.updateMovie(id, docMovie)
+            res.json({
+                _id: id,
+                message: 'Updated successfully'
+            })
+        }catch(error){
+            console.error('Error updating the movie: ', error)
+            throw error
+        }
+    }
+
+    async deleteMovie(req, res){
+        try{
+            const { id } = req.params
+            const movie = await movieModel.deleteMovie(id)
+            res.json({
+                _id: id,
+                message: 'Movie deleted successfully'
+            })
+        }catch(error){
+            console.error('Error deleting a movie: ', error)
+            throw error;
+        }
+    }
 }

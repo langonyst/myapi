682806252b7ca4ab576192971b1a1bcd0f7ab152
Langony ST <langonyst@gmail.com>
2023-06-16 12:13:28 -0300

    feat: adicionado funções de crud para models

--- src/models/MoviesModel.js
+++ src/models/MoviesModel.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import connectToMongo from "../db/dbConnect.js";
 
 export class MoviesModel {
@@ -9,6 +10,53 @@ export class MoviesModel {
         }catch(error){
             console.error('Error listing movies: ', error)
             throw error;
+        }
+    }
+
+    async findMovieById(id) {
+        try{
+            const db = await connectToMongo()
+            const idMovie = new ObjectId(id)
+            const movie = await db.collection('movies').findOne({_id: idMovie})
+            return movie
+        }catch(error){
+            console.error('Error find movie: ', error)
+            throw error;
+        }
+    }
+
+    async createMovie(doc){
+        try{
+            const db = await connectToMongo()
+            const movie = await db.collection('movies').insertOne(doc)
+            return movie
+        }catch(error){
+            console.error('Error creating a movie: ', error)
+            throw error
+        }
+    }
+
+    async updateMovie(id, doc){
+        try{
+            const db = await connectToMongo()
+            const idMovie = new ObjectId(id)
+            const movie = await db.collection('movies').updateOne({_id: idMovie}, {$set: doc})
+            return movie
+        }catch(error){
+            console.error('Error updating the movie: ', error)
+            throw error
+        }
+    }
+
+    async deleteMovie(id){
+        try{
+            const db = await connectToMongo()
+            const idMovie = new ObjectId(id)
+            await db.collection('movies').deleteOne({_id: idMovie})
+            return true
+        }catch(error){
+            console.error('Error deleting a movie: ', error)
+            throw error
         }
     }
 }

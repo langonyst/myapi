24234848121e5d956268df0eeb2ecc0937c8889a
Langony ST <langonyst@gmail.com>
2023-06-16 12:52:43 -0300

    feat: adicionado rota e configuração para documentação swagger

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import movieRouter from './routes/moviesRoutes.js'
+import swaggerRouter from './routes/swagger.cjs'
 
 const app = express()
 
@@ -7,7 +8,14 @@ const port = 3000 || process.env.PORT
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.status(200).send({
+        message: 'Para visualizar os endpoints acesse http://link.com/doc'
+    })
+})
+
 app.use('/movies', movieRouter)
+app.use('/doc', swaggerRouter)
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)

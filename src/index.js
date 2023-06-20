e93a5466a8afb644c230c6e056f7dd25a0bfd83f
Langony ST <langonyst@gmail.com>
2023-06-20 11:02:46 -0300

    feat: adicionado endpoint para noticias

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import movieRouter from './routes/moviesRoutes.js'
 import userRouter from './routes/usersRoutes.js'
 import swaggerRouter from './routes/swagger.cjs'
+import newsRouter from './routes/NewsRoutes.js'
 
 const app = express()
 
@@ -17,6 +18,7 @@ app.get('/', (req, res) => {
 
 app.use('/movies', movieRouter)
 app.use('/users', userRouter)
+app.use('/news', newsRouter)
 app.use('/doc', swaggerRouter)
 
 app.listen(port, () => {

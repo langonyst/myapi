0ee9e80d07b64a2855bec0b8b2674bcf1aa26852
Langony ST <langonyst@gmail.com>
2023-06-20 11:02:18 -0300

    feat: adicionado rotas para noticias

---
+++ src/routes/NewsRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import { NewsController } from '../controllers/NewsController.js'
+import { validId } from '../middlewares/global.js'
+
+const newsRouter = express.Router()
+const newsController = new NewsController()
+
+newsRouter.get('/', newsController.getNews)
+newsRouter.get('/:id', validId, newsController.getNewsById)
+newsRouter.post('/', newsController.createNews)
+newsRouter.patch('/:id', validId, newsController.updateNews)
+newsRouter.delete('/:id', validId, newsController.deleteNews)
+
+export default newsRouter
